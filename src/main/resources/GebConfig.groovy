313661a07ec9eb9e478df050111fcf891a3a94de
Kishen Simbhoedatpanday <ksimbhoedatpanday@xebia.com>
2014-10-04 11:39:09 +0200

    Cleanup

--- src/main/resources/GebConfig.groovy
+++ src/main/resources/GebConfig.groovy
@@ -4,9 +4,6 @@ import org.openqa.selenium.firefox.FirefoxDriver
 driver = { new FirefoxDriver() }
 
 environments {
-
-    // run as “grails -Dgeb.env=firefox test-app”
-    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
     chrome {
         driver = { new ChromeDriver() }
     }
